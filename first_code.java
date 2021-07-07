3cb54de5135e2bb608b23e8ff87a0c3e0f5c7924
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-07-07 14:30:16 +0530

    Add files via upload

---
+++ first_code.java
@@ -0,0 +1,10 @@
+
+public class first_code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Ayush and this is my first code in java");
+
+	}
+
+}
